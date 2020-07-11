56d6ca4a5b854405ec6994078ad66a6007d7c171
Heather Beckman <heatherbeckman85@gmail.com>
2020-07-11 22:42:40 +0000

    Completed Task 1

--- index.js
+++ index.js
@@ -19,15 +19,27 @@ const worldCup2014 = fifaData.filter(element => element["Year"] === 2014);
 
 const final2014 = worldCup2014.filter(element => element["Stage"] === "Final");
 
-// console.log(final2014);
+  console.log(final2014);
 
 const homeTeam = final2014[0]['Home Team Name'];
 
-console.log(homeTeam);
+  console.log(homeTeam);
 
 const awayTeam = final2014[0]['Away Team Name'];
 
-console.log(awayTeam);
+  console.log(awayTeam);
+
+const homeGoals = final2014[0]['Home Team Goals'];
+
+  console.log(homeGoals);
+
+const awayGoals = final2014[0]['Away Team Goals'];
+
+  console.log(awayGoals);
+
+const winner = final2014[0]['Win conditions'];
+
+  console.log(winner);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
