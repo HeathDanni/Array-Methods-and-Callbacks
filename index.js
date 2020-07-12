a274304ddc85f21b00ccfaacc52346bcdb741895
Heather Beckman <heatherbeckman85@gmail.com>
2020-07-12 00:10:51 +0000

    Task 3 (question)

--- index.js
+++ index.js
@@ -53,13 +53,14 @@ function getFinals(data) {
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears(func) {
 
-    /* code here */
+    let years = func.map(element => element["Year"]);
+    return years;
 
 };
 
-getYears();
+console.log(getYears(getFinals(fifaData)));
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
